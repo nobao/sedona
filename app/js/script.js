5a6167a7f05d5e848b11ab5acbbb9d76de746a3e
nobao <nobao.code@gmail.com>
2020-05-17 23:51:12 +0300

    Закончил html верстку страницы hotels.html

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,8 @@
 let hotelSearchOpenButton = document.querySelector(".hotel-search-title"),
-    hotelSearchForm = document.querySelector(".hotel-search-form-wrapper");
+  hotelSearchForm = document.querySelector(".hotel-search-form-wrapper");
 
-hotelSearchOpenButton.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    hotelSearchForm.classList.toggle("hide-item");
-    hotelSearchForm.classList.toggle("pull-down");
+hotelSearchOpenButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  hotelSearchForm.classList.toggle("hide-item");
+  hotelSearchForm.classList.toggle("pull-down");
 });
